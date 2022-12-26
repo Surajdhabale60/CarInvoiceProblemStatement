b14fb17b824ab41d4d92babcde0ebc8e0c314d03
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-26 09:22:04 +0530

    [Suraj]Add:To Enchanced Invoice Summary

---
+++ CarInvoiceProblem/CarInvoiceProblem/EnhancedInvoiceSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInvoiceProblem
+{
+    public class EnhancedInvoiceSummary
+    {
+        public double totalNumberOfRides;
+        public double totalFare;
+        public double averageFarePerRide;
+
+        public EnhancedInvoiceSummary(double totalFare, double totalNumberOfRides)
+        {
+            this.totalFare = totalFare;
+            this.totalNumberOfRides = totalNumberOfRides;
+            this.averageFarePerRide = this.totalFare / this.totalNumberOfRides;
+        }
+    }
+}
