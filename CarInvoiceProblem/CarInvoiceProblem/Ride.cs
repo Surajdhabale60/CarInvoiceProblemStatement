07e92eaa2e6ec978b73a89466bc2e4bfe224cd52
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-26 09:08:09 +0530

    [Suraj]Add:Calculate Aggrigate Fare For Multiple Ride

---
+++ CarInvoiceProblem/CarInvoiceProblem/Ride.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInvoiceProblem
+{
+    public class Ride
+    {
+        public double distance { get; set; }
+        public double time { get; set; }
+
+        public Ride(double distance, double time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
--- CarInvoiceProblem/CarInvoiceProblemTest/UnitTest1.cs
+++ CarInvoiceProblem/CarInvoiceProblemTest/UnitTest1.cs
@@ -20,10 +20,18 @@ namespace CarInvoiceProblemTest
         }
         [Test]
         //Test to get Mininum Fare when given less than minimum fare
-        public void GivenDistanceAndTimeForSingleRide_LessThanMinFare_ShouldReturnMinimumFare()
+        public void GivenProperDistanceAndTimeForSingleRide_LessThanMinFare_ShouldReturnMinimumFare()
         {
             double CabFare = cabInvoiceGenerator.CalculateFare(0.1, 0.5);
             Assert.AreEqual(5, CabFare);
+        }
+        [Test]
+        //Test to get aggregate fare for multiple rides
+        public void GivenProperDistanceAndTimeForMultipleRide_ShouldReturnAggregateFare()
+        {
+            Ride[] ride = { new Ride(3.0, 5.0), new Ride(2.0, 5.0), new Ride(0.1, 0.5) };
+            double INVOICE_SUMMARY = this.cabInvoiceGenerator.GetMultipleRideFare(ride);
+            Assert.AreEqual(65, INVOICE_SUMMARY);
         }
     }
 }
