19ae028832b5a64524912f184f57b31bcc50e609
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-26 09:01:47 +0530

    [Suraj]Add:Calculate Total Fare For Single Ride

---
+++ CarInvoiceProblem/CarInvoiceProblem/CabInvoiceGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInvoiceProblem
+{
+    public class CabInvoiceGenerator
+    {
+        private static readonly double COST_PER_KILOMETER = 10.0;
+        private static readonly double COST_PER_MINUTE = 1.0;
+        private static readonly double MINIMUM_FARE = 5.0;
+        private double CAB_FARE = 0.0;
+
+
+        public double CalculateFare(double distance, double time)
+        {
+            this.CAB_FARE = (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
+            return Math.Max(this.CAB_FARE, MINIMUM_FARE);
+        }
+    }
+}
---
+++ CarInvoiceProblem/CarInvoiceProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarInvoiceProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cab Ride Problem Statement for testing");
+        }
+    }
+}
